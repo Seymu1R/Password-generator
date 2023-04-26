95e0054297fe320a43fb4ce40ba80e2ae6caf51c
Seymu1R <Seymuram@code.edu.az>
2023-04-26 22:25:35 +0400

    first commit

---
+++ PassGenerator.cs
@@ -0,0 +1,57 @@
+﻿namespace Ipassgenerator
+{
+    public class PassGenerator
+    {
+        private string digits = "0123456789";
+        private string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        private string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        private string GeneratePassword(string characterSet, int length)
+        {
+            Random random = new Random();
+            string password = new string(Enumerable.Repeat(characterSet, length).Select(s => s[random.Next(s.Length)]).ToArray());
+            return password;
+        }
+
+        public string GenerateNumericPassword(int length)
+        {
+            string characterSet = digits;
+            return GeneratePassword(characterSet, length);
+        }
+
+        public string GenerateLetterPassword(int length)
+        {
+            string characterSet = letters;
+            return GeneratePassword(characterSet, length);
+        }
+
+        public string GenerateMixedPassword(int length)
+        {
+            string characterSet = characters;
+            return GeneratePassword(characterSet, length);
+        }
+
+        public string Digits
+        {
+            get { return digits; }
+            set { digits = value; }
+        }
+
+        public string Letters
+        {
+            get { return letters; }
+            set { letters = value; }
+        }
+
+        public string Characters
+        {
+            get { return characters; }
+            set
+            {
+                characters = value;
+                digits = new string(characters.Where(char.IsDigit).Distinct().ToArray());
+                letters = new string(characters.Where(char.IsLetter).Distinct().ToArray());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Ipassgenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zəhmət olmasa şifrənin uzunluğunu girin: ");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("1) Password yalnız reqemlərdən ibarət olsun. ");
+            Console.WriteLine("2) Password yalnız herflerdən ibarət olsun. ");
+            Console.WriteLine("3) Password hem herflerden hem reqemlerdən ibarət olsun. ");
+            Console.Write("Zəhmət olmasa seçiminizi edin (1-3): ");
+            int choice = int.Parse(Console.ReadLine());
+
+            PassGenerator passGenerator = new PassGenerator();
+
+            string password = "";
+            switch (choice)
+            {
+                case 1:
+                    password = passGenerator.GenerateNumericPassword(length);
+                    break;
+                case 2:
+                    password = passGenerator.GenerateLetterPassword(length);
+                    break;
+                case 3:
+                    password = passGenerator.GenerateMixedPassword(length);
+                    break;
+                default:
+                    Console.WriteLine("Nəsə düz getmədi.");
+                    break;
+            }
+
+            Console.WriteLine("Passüord: " + password);
+
+        }
+    }
+}
